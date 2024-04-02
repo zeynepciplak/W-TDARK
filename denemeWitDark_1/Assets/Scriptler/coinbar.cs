f6200120114ae782eda3934b2156fb835f67f2f2
Zeynep Cıplak <128297760+zeynepciplak@users.noreply.github.com>
2024-04-02 06:33:12 +0300

    coinbar yapıldı script başlangıcı atıldı

---
+++ denemeWitDark_1/Assets/Scriptler/coinbar.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class coinbar : MonoBehaviour
+{
+    public float maxCoin;
+    float currentCoin;
+    public Image coinbar;
+    // Use this for initialization
+    void Start()
+    {
+        currentCoin = maxCoin;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(currentCoin>=maxCoin)
+        {
+            currentCoin = maxCoin;
+        }
+        coinbar.fillAmount = currentCoin / 100;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Item"))
+        {
+            currentCoin -= collision.GetComponent<Item>().damage;
+
+
+            if(currentCoin<=0)
+            {
+                currentCoin = 0;
+                Destroy(gameObject);
+            }
+        }
+    }
+}
