96bcc1d552edcf5e9780f532122f43bde96f3153
Mustafa Ornek <2003013014@student.beykent.edu.tr>
2024-04-09 01:51:11 +0300

    bacground müziği

--- denemeWitDark_1/Assets/Scriptler/PlayerCtrl.cs
+++ denemeWitDark_1/Assets/Scriptler/PlayerCtrl.cs
@@ -12,10 +12,10 @@ public class PlayerCtrl : MonoBehaviour
     public static float movSpeed;
     public static float speedX, speedY;
     Rigidbody2D rb;
-    bool isWalkingOnGrass;
-    bool isWalkingOnStone;
+    bool isWalkingOnGrass; // Çalı üzerinde mi yürünüyor?
+    bool isWalkingOnStone; // Taş üzerinde mi yürünüyor?
 
-    bool isWalkingOnForest;
+    bool isWalkingOnForest; // Orman zemininde mi yürünüyor?
 
     public AudioManager audioManager;
 
