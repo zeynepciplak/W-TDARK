51dcfdf0bafecc5abce980d3ec78a0a265abf2b8
C0rdur0y <816096faruket@gmail.com>
2024-04-19 23:04:26 +0300

    Sprite-default

--- denemeWitDark_1/Assets/Scriptler/Rotator.cs
+++ denemeWitDark_1/Assets/Scriptler/Rotator.cs
@@ -1,19 +1,3 @@
-/*
-using UnityEngine;
-
-public class Rotator : MonoBehaviour
-{
-    public float rotationSpeed = 100f; // Dönme hýzý (derece/saniye)
-
-    void Update()
-    {
-        // Her saniye, transform.Rotate() fonksiyonu ile nesneyi Z ekseni etrafýnda döndürürüz.
-        transform.Rotate(Vector3.forward * -rotationSpeed * Time.deltaTime);
-    }
-}
-*/
-
-
 using System.Collections;
 using UnityEngine;
 
@@ -61,4 +45,3 @@ public class Rotator : MonoBehaviour
         donuyorMu = false;
     }
 }
-
